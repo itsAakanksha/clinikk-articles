2216ade6892547714fbb3eead7ca785f957b4c98
Aakanksha <82.25aakanksha@gmail.com>
2024-06-22 11:51:31 +0530

    added cta

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
    <>
-   <div className='border-y-2 border-gray-300 w-full flex justify-center '>
+   <div className='border-y-2 border-gray-300 w-full flex justify-center pt-4 '>
    <h1 className='mx-auto md:text-6xl p-3 text-4xl text-center '>Treatments We Provide</h1>
    </div>
    </>
