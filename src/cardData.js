4d6272f0939080393d3d87b00eb3109141178d70
Aakanksha <82.25aakanksha@gmail.com>
2024-06-22 17:34:32 +0530

    link

--- src/cardData.js
+++ src/cardData.js
@@ -34,21 +34,21 @@ const cardData = [
   {
     categoryName:"Skin Conditions we treat",
     type:[ {
-      id: 3,
+      id: 6,
       title: ' Allergic reactions',
       description:" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       imageUrl: 'https://plus.unsplash.com/premium_photo-1661537390414-33d7ad495159?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       date: "Sunday, 1 Jan 2023",
     },
     {
-      id: 4,
+      id: 6,
       title: 'Dermatitis',
       description:" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       imageUrl: 'https://plus.unsplash.com/premium_photo-1661475615346-0ec058fec898?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       date: "Sunday, 1 Jan 2023",
     },
     {
-      id: 5,
+      id: 6,
       title: ' Fungal Infections',
       description:" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       imageUrl: 'https://plus.unsplash.com/premium_photo-1677545889722-423483ddd9f9?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
@@ -73,7 +73,7 @@ const cardData = [
       date: "Sunday, 1 Jan 2023",
     },
     {
-      id: 6,
+      id: 5,
       title: 'Headaches',
       description:" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       imageUrl: 'https://images.unsplash.com/photo-1618498390344-445c804f2ac5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
@@ -84,21 +84,21 @@ const cardData = [
    {
     categoryName:"Pain Management",
     type:[ {
-      id: 5,
+      id: 3,
       title: ' Joint Pain',
       description:" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       imageUrl: 'https://plus.unsplash.com/premium_photo-1664910605048-44c8450c0356?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       date: "Sunday, 1 Jan 2023",
     },
     {
-      id: 5,
+      id: 3,
       title: ' Back Pain',
       description:" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       imageUrl: 'https://plus.unsplash.com/premium_photo-1661779360652-b067e858ad02?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       date: "Sunday, 1 Jan 2023",
     },
     {
-      id: 6,
+      id: 3,
       title: 'Body Aches',
       description:" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       imageUrl: 'https://images.unsplash.com/photo-1585917138424-61cf3ee524d9?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
@@ -115,14 +115,14 @@ const cardData = [
       date: "Sunday, 1 Jan 2023",
     },
     {
-      id: 5,
+      id: 4,
       title: 'Urinary Tract Infections (UTI)',
       description:" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       imageUrl: 'https://plus.unsplash.com/premium_photo-1702599099904-4f25f14b0471?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       date: "Sunday, 1 Jan 2023",
     },
     {
-      id: 6,
+      id: 4,
       title: ' Wound Infections.',
       description:" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       imageUrl: 'https://images.unsplash.com/photo-1579544758184-a8994eb607a4?q=80&w=1930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
