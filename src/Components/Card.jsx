80015d4159374d6199354738f8f2c43fa17a9e78
Aakanksha <82.25aakanksha@gmail.com>
2024-06-26 14:17:41 +0530

    done

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,31 +1,25 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
 
 function Card({ item }) {
-  const navigate = useNavigate();
-  const {id} = item
-  const handleClick = () => {
-    navigate(`/details/${id}`);
-  };
+  const { id } = item;
 
   return (
-    <div className='w-[95vw] md:w-[384px] min-h-[456px] flex flex-col cursor-pointer mx-2' onClick={handleClick}>
+    <Link to={`/details/${id}`} className='w-[95vw] md:w-[384px] min-h-[456px] flex flex-col cursor-pointer mx-2'>
       <div className='h-[225px]'>
         <img 
           src={item.imageUrl} 
           className='h-full w-full object-cover rounded-t-3xl' 
-          alt="" 
+          alt={item.title} 
         />
       </div>
-      <div className='flex flex-col gap-2 p-4 flex-grow '>
-       
+      <div className='flex flex-col gap-2 p-4 flex-grow'>
         <h3 className='text-2xl font-semibold'>{item.title}</h3>
         <p className='text-[#667085] line-clamp-6'>
           {item.description}
         </p>
       </div>
-    </div>
+    </Link>
   );
 }
 
